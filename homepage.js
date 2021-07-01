75a06131a77896ba4317a759dbf329cc4a6ea0b6
bambi <75845821+anniezhou19@users.noreply.github.com>
2021-07-01 16:25:30 +0200

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -4,48 +4,76 @@ $(document).ready(function() {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector("#a1").onclick=function(){
-        document.getElementById('a1').style.backgroundColor = 'red';
-        document.getElementById('question').innerHTML = "Incorrect";
-    };
-    document.querySelector("#a2").onclick=function(){
-        document.getElementById('a2').style.backgroundColor = 'red';
-        document.getElementById('question').innerHTML = "Incorrect";
-    };
-    document.querySelector("#a3").onclick=function(){
-        document.getElementById('a3').style.backgroundColor = 'red';
-        document.getElementById('question').innerHTML = "Incorrect";
-    };
-    document.querySelector("#a4").onclick=function(){
-        document.getElementById('a4').style.backgroundColor = 'green';
-        document.getElementById('question').innerHTML = "Correct";
-    };
-    document.querySelector("#a5").onclick=function(){
-        document.getElementById('a5').style.backgroundColor = 'red';
-        document.getElementById('question').innerHTML = "Incorrect";
-    };
+
+$(document).ready(function()
+{
+    $('#a1').click(function()
+    {
+        alert("Correct");
+    });
+
+    $('#a2').click(function()
+    {
+        alert("Incorrect");
+    });
+     $('#a3').click(function()
+    {
+        alert("Incorrect");
+    });
+     $('#a4').click(function()
+    {
+        alert("Incorrect");
+    });
+    $('#a5').click(function()
+    {
+        alert("Incorrect");
+    });
+
+    $('#a6').click(function()
+    {
+        alert("Correct");
+    });
+     $('#a7').click(function()
+    {
+        alert("Incorrect");
+    });
+     $('#a8').click(function()
+    {
+        alert("Incorrect");
+    });
+    $('#a9').click(function()
+    {
+        alert("Incorrect");
+    });
+
+    $('#a10').click(function()
+    {
+        alert("Incorrect");
+    });
+     $('#a11').click(function()
+    {
+        alert("Correct");
+    });
+     $('#a12').click(function()
+    {
+        alert("Incorrect");
+    });
+    $('#a13').click(function()
+    {
+        alert("Incorrect");
+    });
+
+    $('#a14').click(function()
+    {
+        alert("Correct");
+    });
+     $('#a15').click(function()
+    {
+        alert("Incorrect");
+    });
+     $('#a16').click(function()
+    {
+        alert("Incorrect");
+    });
 });
 
-document.addEventListener('DOMContentLoaded', function(){
-    document.querySelector("#a6").onclick=function(){
-        document.getElementById('a6').style.backgroundColor = 'red';
-        document.getElementById('question2').innerHTML = "Incorrect";
-    };
-    document.querySelector("#a7").onclick=function(){
-        document.getElementById('a7').style.backgroundColor = 'green';
-        document.getElementById('question2').innerHTML = "Correct";
-    };
-    document.querySelector("#a8").onclick=function(){
-        document.getElementById('a8').style.backgroundColor = 'red';
-        document.getElementById('question2').innerHTML = "Incorrect";
-    };
-    document.querySelector("#a9").onclick=function(){
-        document.getElementById('a9').style.backgroundColor = 'red';
-        document.getElementById('question2').innerHTML = "Incorrect";
-    };
-    document.querySelector("#a10").onclick=function(){
-        document.getElementById('a10').style.backgroundColor = 'red';
-        document.getElementById('question2').innerHTML = "Incorrect";
-    };
-});
